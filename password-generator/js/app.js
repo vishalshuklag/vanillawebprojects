aeee186a9efe1a95f6f1a96de4f84e5589b38b32
vishalshuklag <er_vishalshukla@outlook.com>
2020-12-31 01:34:27 +0530

    Added copy to clipboard functionality

--- password-generator/js/app.js
+++ password-generator/js/app.js
@@ -1,16 +1,25 @@
 const input = document.querySelector("#password");
 const button = document.querySelector("#btn")
+const copyImg = document.querySelector('.copy');
+const alertDiv = document.querySelector('.alert');
 const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@$+_-?!";
 const passwordLen = 16;
-
+let password = '';
 function generatePassword() {
-    let password = '';
+    password = '';
     for (let index = 0; index < passwordLen; index++) {
         let randomNum = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNum, randomNum + 1);
-
     }
     input.value = password;
 }
 
+function copyPassword() {
+    input.select();
+    input.setSelectionRange(0, 9999);
+    document.execCommand('copy');
+}
+
+
 button.addEventListener('click', generatePassword)
+copyImg.addEventListener('click', copyPassword)
